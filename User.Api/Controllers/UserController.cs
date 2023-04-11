9e2d8967f9856e7c2d235347cc8df7ebf90ce887
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-04-11 18:06:54 +0300

    added new services

--- User.Api/Controllers/UserController.cs
+++ User.Api/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using Events.User;
+using Grpc.Auth.ClientServices;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NetDevPack.Mediator;
 using Newtonsoft.Json;
@@ -14,16 +16,19 @@ namespace User.Api.Controllers
 	{
 		private readonly IUserAppService _userAppService;
 		private readonly IMediatorHandler _mediator;
+		private readonly AuthGrpcService authGrpcService;
 
-		public UserController(IUserAppService userAppService, IMediatorHandler mediator)
+		public UserController(IUserAppService userAppService, IMediatorHandler mediator, AuthGrpcService authGrpcService)
 		{
 			_userAppService = userAppService;
 			_mediator = mediator;
+			this.authGrpcService = authGrpcService;
 		}
 
 		[HttpGet]
 		public async Task<IActionResult> Index()
 		{
+
 			var user = new CreateUserDto
 			{
 				Email = "Test@gmail.com",
@@ -36,10 +41,23 @@ namespace User.Api.Controllers
 
 			//var Event = new UserDeletedEvent(Guid.Parse("60169185-2ba3-4aff-8e26-08db2ed52c4d"));
 			//await _mediator.PublishEvent(Event);
-
 			var history =await _userAppService.GetAllHistoryAsync(Guid.Parse("64169185-2ba3-4aff-8e26-08db2ed52c4d"));
 
 			return Created("Test",history);
+		}
+		[HttpGet,Route("tokentest/{token}"),AllowAnonymous]
+		public async Task<IActionResult> Gettomen(string token)
+		{
+			var user = await authGrpcService.CheckTokenRefreshAndUserId(token);
+
+			return Ok(user);
+		}
+
+		[HttpGet,Route("melih")]
+		public async Task<IActionResult> GetMelih()
+		{
+			//var user = this.HttpContext.User;
+			return Ok("melih bey");
 		}
 	}
 }
