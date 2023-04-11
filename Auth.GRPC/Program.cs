49d44a42bdd8b4dfcae4444e7b11001f8b0f3d2d
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-04-11 18:06:35 +0300

    added services

--- Auth.GRPC/Program.cs
+++ Auth.GRPC/Program.cs
@@ -1,17 +1,36 @@
+using Auth.Application.Extensions;
+using Auth.Domain.Extensions;
 using Auth.GRPC.Services;
+using Auth.Infrastructure.Extensions;
+using Events.Stores.MongoDb;
+using Redis.OM;
 
-var builder = WebApplication.CreateBuilder(args);
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+		builder.WebHost.UseUrls("https://localhost:8065");
+		// Additional configuration is required to successfully run gRPC on macOS.
+		// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
-// Add services to the container.
-builder.Services.AddGrpc();
+		// Add services to the container.
+		builder.Services.UseDomain(typeof(Program));
+		builder.Services.AddMongoDbEventStore(builder.Configuration);
+		builder.Services.AddSingleton(new RedisConnectionProvider(builder.Configuration["REDIS_CONNECTION_STRING"]));
+		builder.Services.UseAuthApplication();
+		builder.Services.UseAuthInfrastructure();
 
-var app = builder.Build();
+		builder.Services.AddGrpc();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-app.Run();
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		app.MapGrpcService<AuthService>();
+		app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+		app.Run();
+	}
+}
