4fd6e1a22e5800231e55c56213069058aee18350
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-04-11 18:06:06 +0300

    added new services

--- Auth.Api/Program.cs
+++ Auth.Api/Program.cs
@@ -1,25 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using Auth.Application.Extensions;
+using Auth.Domain.Extensions;
+using Auth.Infrastructure.Extensions;
+using Events.Stores.MongoDb;
+using Microsoft.AspNetCore.Builder;
+using Redis.OM;
+using Swagger;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+		builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddMySwagger(builder.Configuration);
+		builder.Services.UseDomain(typeof(Program));
+		builder.Services.AddMongoDbEventStore(builder.Configuration);
+		builder.Services.AddSingleton(new RedisConnectionProvider(builder.Configuration["REDIS_CONNECTION_STRING"]));
+		builder.Services.UseAuthApplication();
+		builder.Services.UseAuthInfrastructure();
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseMySwagger(app.Configuration);
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+
+		app.Run();
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
