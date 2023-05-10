420c29b31ff221edf109e34ff5f71a59d5ce459f
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-10 17:23:29 +0300

    PublishDomainEvents function moved

--- User.Infrastructure/Context/UserContext.cs
+++ User.Infrastructure/Context/UserContext.cs
@@ -1,4 +1,5 @@
-﻿using Database.Infrastructure;
+﻿using Database.Extensions;
+using Database.Infrastructure;
 using FluentValidation.Results;
 using Microsoft.EntityFrameworkCore;
 using NetDevPack.Domain;
@@ -56,28 +57,4 @@ namespace User.Infrastructure.Context
 		}
 	}
 
-	public static class MediatorExtension
-	{
-		public static async Task PublishDomainEvents<T>(this IMediatorHandler mediator, T ctx) where T : DbContext
-		{
-			var domainEntities = ctx.ChangeTracker
-				.Entries<Entity>()
-				.Where(x => x.Entity.DomainEvents != null && x.Entity.DomainEvents.Any());
-
-			var domainEvents = domainEntities
-				.SelectMany(x => x.Entity.DomainEvents)
-				.ToList();
-
-			domainEntities.ToList()
-				.ForEach(entity => entity.Entity.ClearDomainEvents());
-
-			var tasks = domainEvents
-				.Select(async (domainEvent) =>
-				{
-					await mediator.PublishEvent(domainEvent);
-				});
-
-			await Task.WhenAll(tasks);
-		}
-	}
 }
