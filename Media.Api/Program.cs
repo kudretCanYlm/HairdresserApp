83a11377b64b8174c83cede8e38f7cb4327ce003
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-11 16:36:02 +0300

    added news

--- Media.Api/Program.cs
+++ Media.Api/Program.cs
@@ -1,19 +1,31 @@
+using Events.Stores.MongoDb;
+using Media.Application.Extensions;
+using Media.Domain.Extensions;
+using Media.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.UseDomain(typeof(Program));
+builder.Services.UseMediaInfrastructure(x => x.UseSqlServer(builder.Configuration.GetConnectionString("MediaServiceDb")));
+builder.Services.UseMediaApplication();
+builder.Services.AddMongoDbEventStore(builder.Configuration);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddMySwagger(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseMySwagger(app.Configuration);
 }
 
 app.UseHttpsRedirection();
