177c326c51613db431d331df83811876fb3fc35f
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-11 16:29:20 +0300

    refactored

--- User.Domain/Models/UserModel.cs
+++ User.Domain/Models/UserModel.cs
@@ -4,32 +4,11 @@ using NetDevPack.Domain;
 
 namespace User.Domain.Models
 {
-	[PrimaryKey(nameof(Id))]
 	public class UserModel:BaseEntity,IAggregateRoot
 	{
 		public UserModel()
 		{
 			Id = Guid.NewGuid();
-		}
-
-		public UserModel(string name, string surname, string email, string password, string phone)
-		{
-			Id=Guid.NewGuid();
-			Name = name;
-			Surname = surname;
-			Email = email;
-			Password = password;
-			Phone = phone;
-		}
-		
-		public UserModel(Guid id,string name, string surname, string email, string password, string phone)
-		{
-			Id = id;
-			Name = name;
-			Surname = surname;
-			Email = email;
-			Password = password;
-			Phone = phone;
 		}
 
 		public string Name { get; set; }
