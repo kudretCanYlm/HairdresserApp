6c7c178c1ad64e081110ebe4d26be3cb2484b799
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-25 16:59:39 +0300

    refactored and added CorrelationId

--- Events/Media/MediaCreatedEvent.cs
+++ Events/Media/MediaCreatedEvent.cs
@@ -2,22 +2,22 @@
 
 namespace Events.Media
 {
-	public class MediaCreatedEvent:Event
+	public class MediaCreatedEvent:Event,MassTransit.CorrelatedBy<Guid>
 	{
 		public MediaCreatedEvent()
 		{
 			
 		}
 
-		public MediaCreatedEvent(Guid id, string fileExtension, byte[] mediaData, string customType, Guid ımageOwnerId)
+		public MediaCreatedEvent(Guid correlationId, Guid id, string fileExtension, byte[] mediaData, string customType, Guid imageOwnerId)
 		{
-			
+			CorrelationId = correlationId;
 			Id = id;
 			AggregateId = id;
 			FileExtension = fileExtension;
 			MediaData = mediaData;
 			CustomType = customType;
-			ImageOwnerId = ımageOwnerId;
+			ImageOwnerId = imageOwnerId;
 		}
 
 		public Guid Id { get; set; }
@@ -25,5 +25,6 @@ namespace Events.Media
 		public byte[] MediaData { get; set; }
 		public string CustomType { get; set; }
 		public Guid ImageOwnerId { get; set; }
+		public Guid CorrelationId { get; }
 	}
 }
