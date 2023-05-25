955d9c55172c9cc86b19e061e377351ba3b07d73
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-25 17:01:17 +0300

    first commit

---
+++ User.Domain/Queries/User/UserLoginQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace User.Domain.Queries.User
+{
+	public class UserLoginQuery:IRequest<Guid?>
+	{
+		public UserLoginQuery()
+		{
+
+		}
+
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
