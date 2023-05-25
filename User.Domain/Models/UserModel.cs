2b096ecda22e9eb25dad36db4d4e7829c961ce60
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-25 17:05:46 +0300

    added new methods

--- User.Domain/Models/UserModel.cs
+++ User.Domain/Models/UserModel.cs
@@ -1,6 +1,7 @@
 ﻿using Database.Entity;
-using Microsoft.EntityFrameworkCore;
 using NetDevPack.Domain;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace User.Domain.Models
 {
@@ -16,6 +17,45 @@ namespace User.Domain.Models
 		public string Email { get; set; }
 		public string Password { get; set; }
 		public string Phone { get; set; }
+
+		public void ToHashPassword()
+		{
+			// Create a SHA256   
+			using (SHA256 sha256Hash = SHA256.Create())
+			{
+				// ComputeHash - returns byte array  
+				byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(this.Password));
+
+				// Convert byte array to a string   
+				StringBuilder builder = new StringBuilder();
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					builder.Append(bytes[i].ToString("x2"));
+				}
+
+				this.Password = builder.ToString();
+			}
+		}
+
+		public static string ToHash(string value)
+		{
+			using (SHA256 sha256Hash = SHA256.Create())
+			{
+				// ComputeHash - returns byte array  
+				byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+				// Convert byte array to a string   
+				StringBuilder builder = new StringBuilder();
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					builder.Append(bytes[i].ToString("x2"));
+				}
+
+				return builder.ToString();
+			}
+		}
+
+		
 
 	}
 }
