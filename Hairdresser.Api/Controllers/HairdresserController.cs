5914f366458372cf8be492238737e3edabd1d093
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-27 16:06:44 +0300

    Refactored And Added New Methods

--- Hairdresser.Api/Controllers/HairdresserController.cs
+++ Hairdresser.Api/Controllers/HairdresserController.cs
@@ -1,9 +1,8 @@
-﻿using Grpc.Auth;
+﻿using Database.PaggingAndFilter;
+using Grpc.Auth;
+using Grpc.User.ClientServices;
 using Hairdresser.Application.Dto;
 using Hairdresser.Application.Interfaces.Hairdresser;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hairdresser.Api.Controllers
@@ -13,14 +12,16 @@ namespace Hairdresser.Api.Controllers
 	public class HairdresserController : ControllerBase
 	{
 		private readonly IHairdresserAppService _hairdresserAppService;
+		private readonly UserGrpcService _userGrpcService;
 
-		public HairdresserController(IHairdresserAppService hairdresserAppService)
+		public HairdresserController(IHairdresserAppService hairdresserAppService, UserGrpcService userGrpcService)
 		{
 			_hairdresserAppService = hairdresserAppService;
+			_userGrpcService = userGrpcService;
 		}
 
-		[HttpPost,Route("AddHairdresser")]
-		public async Task<IActionResult> AddHairdresser([FromBody]CreateHairdresserDto? createHairdresserDto)
+		[HttpPost, Route("AddHairdresser")]
+		public async Task<IActionResult> AddHairdresser([FromBody] CreateHairdresserDto? createHairdresserDto)
 		{
 			createHairdresserDto.OwnerId = (Guid)GrpcAuthExtension.GetCurrentUserId(HttpContext);
 
@@ -28,12 +29,36 @@ namespace Hairdresser.Api.Controllers
 			return Ok(result);
 		}
 
-		[HttpGet,Route("GetAllHairdressers")]
+		[HttpGet, Route("GetAllHairdressers")]
 		public async Task<IActionResult> GetAllHairdressers()
 		{
 			var result = await _hairdresserAppService.GetAllHairdressers();
 
 			return Ok(result);
+		}
+
+		[HttpPost, Route("GetAllHairdressersByUserAddress")]
+		public async Task<IActionResult> GetAllHairdressersByUserAddress([FromBody] PageSearchArgs pageSearchArgs)
+		{
+			var userId = (Guid)GrpcAuthExtension.GetCurrentUserId(HttpContext);
+
+			var address = await _userGrpcService.GetSelectedUserAddressById(userId);
+
+			var filter = new FilteringOption
+			{
+				Field = "location",
+				Operator = FilteringOperator.Contains,
+				Value = address.City + " " + address.State
+			};
+
+			pageSearchArgs.FilteringOptions.RemoveAll(x => x.Field == "location");
+
+			pageSearchArgs.FilteringOptions.Add(filter);
+
+			var result = await _hairdresserAppService.GetAllHairdresserByFilter(pageSearchArgs);
+
+			return Ok(result);
+
 		}
 
 	}
