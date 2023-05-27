bb4b4e04610dc4e1fb062783369530af921ef047
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-27 16:02:47 +0300

    Rename And Move

---
+++ Events/Media/MediaSuccessfulEvent.cs
@@ -0,0 +1,19 @@
+﻿using NetDevPack.Messaging;
+
+namespace Events.Media
+{
+    public class MediaSuccessfulEvent : Event, MassTransit.CorrelatedBy<Guid>
+    {
+        public MediaSuccessfulEvent(Guid correlationId, Guid id, Guid ownerId)
+        {
+            CorrelationId = correlationId;
+            Id = id;
+            OwnerId = ownerId;
+        }
+
+        public Guid Id { get; set; }
+        public Guid OwnerId { get; set; }
+
+        public Guid CorrelationId { get; }
+    }
+}
