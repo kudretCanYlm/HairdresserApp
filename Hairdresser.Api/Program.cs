39b5269969d3b40e823b0e0140f311e7f1f906f8
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-27 16:07:46 +0300

    Add Grpc And Saga

--- Hairdresser.Api/Program.cs
+++ Hairdresser.Api/Program.cs
@@ -1,7 +1,11 @@
+using Events.MassTransitOptions;
 using Events.Stores.MongoDb;
 using Grpc.Auth;
+using Grpc.Media;
+using Grpc.User;
 using Hairdresser.Application.Extensions;
 using Hairdresser.Domain.Extensions;
+using Hairdresser.Domain.Sagas.CreateHairdresserMedia;
 using Hairdresser.Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Swagger;
@@ -11,6 +15,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.UseDomain(typeof(Program));
 builder.Services.AddAuthGrpc(builder.Configuration["AuthUrl"]);
+builder.Services.AddUserGrpc(builder.Configuration["UserUrl"]);
+builder.Services.AddMediaGrpc(builder.Configuration["MediaUrl"]);
+builder.Services.AddMyMassTransitStateMachine<HairdresserMediaStateMachine, HairdresserMediaStateInstance>(builder.Configuration, RabbitMqQueues.StateMachine_HairdresserMedia);
 builder.Services.UseHairdresserInfrastructure(x => x.UseSqlServer(builder.Configuration.GetConnectionString("HairdresserServiceDb")));
 builder.Services.UseHairdresserApplication();
 builder.Services.AddMongoDbEventStore(builder.Configuration);
