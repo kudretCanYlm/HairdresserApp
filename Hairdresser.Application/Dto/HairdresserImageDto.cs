f304fac54cb9579873840a2575786780470760c5
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-27 16:08:47 +0300

    first commit

---
+++ Hairdresser.Application/Dto/HairdresserImageDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Hairdresser.Application.Dto
+{
+	public class HairdresserImageDto
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string? About { get; set; }
+		public string Address { get; set; }
+		public string Coordinate { get; set; }
+		public bool IsOpenNow { get; set; }
+		public TimeSpan? OpenHour { get; set; }
+		public TimeSpan? CloseHour { get; set; }
+		public string? WorkDays { get; set; }
+		public Guid OwnerId { get; set; }
+		public string Base64Media { get; set; }
+	}
+}
