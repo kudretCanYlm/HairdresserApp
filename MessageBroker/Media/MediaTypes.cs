4ff3d064cac363a91c333a20ea6936b8a086be7d
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-28 16:15:56 +0300

    Added News

--- MessageBroker/Media/MediaTypes.cs
+++ MessageBroker/Media/MediaTypes.cs
@@ -7,6 +7,8 @@
 		}
 
 		public const string USER_PROFILE_IMAGE = "USER_PROFILE_IMAGE";
+		public const string HAIRDRESSER_SINGLE = "HAIRDRESSER_SINGLE";
+		public const string HAIRDRESSER_SERVICE_MULTI = "HAIRDRESSER_SERVICE_MULTI";
 
 	}
 }
