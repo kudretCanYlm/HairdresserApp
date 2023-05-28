72a3a03e03dfccd7d951ff866c24bd1218db0fa5
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-28 16:24:21 +0300

    Add Grpc And MassTransit

--- HairdresserService.Api/Program.cs
+++ HairdresserService.Api/Program.cs
@@ -1,7 +1,11 @@
+using Events.MassTransitOptions;
 using Events.Stores.MongoDb;
 using Grpc.Auth;
+using Grpc.Hairdresser;
+using Grpc.Media;
 using HairdresserService.Application.Extensions;
 using HairdresserService.Domain.Extensions;
+using HairdresserService.Domain.Sagas.CreateHairdresserServiceMedia;
 using HairdresserService.Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Swagger;
@@ -9,12 +13,14 @@ using Swagger;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.UseDomain(typeof(Program));
 builder.Services.AddAuthGrpc(builder.Configuration["AuthUrl"]);
+builder.Services.AddHairdresserGrpc(builder.Configuration["HairdresserUrl"]);
+builder.Services.AddMediaGrpc(builder.Configuration["MediaUrl"]);
+builder.Services.UseDomain(typeof(Program));
+builder.Services.AddMyMassTransitStateMachine<HairdresserServiceMediaStateMachine, HairdresserServiceMediaStateInstance>(builder.Configuration, RabbitMqQueues.StateMachine_HairdresserServiceMedia);
 builder.Services.AddHairdresserServiceInfrastructure(x => x.UseSqlServer(builder.Configuration.GetConnectionString("HairdresserServiceServiceDb")));
 builder.Services.AddHairdresserServiceApplication();
 builder.Services.AddMongoDbEventStore(builder.Configuration);
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
