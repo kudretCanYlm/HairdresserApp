45d6b23f569dc9722226a46e98ce8380167b81e6
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-28 16:18:08 +0300

    Refactor

--- Media.Api/Integrations/Consumers/MediaCreatedEventConsumer.cs
+++ Media.Api/Integrations/Consumers/MediaCreatedEventConsumer.cs
@@ -1,39 +1,34 @@
-﻿using Events.MassTransitOptions;
-using Events.Media;
-using Events.User;
+﻿using Events.Media;
 using MassTransit;
 using MediatR;
-using NetDevPack.Mediator;
 
 namespace Media.Api.Integrations.Consumers
 {
 	public class MediaCreatedEventConsumer : IConsumer<MediaCreatedEvent>
 	{
 		private readonly ILogger<MediaCreatedEventConsumer> _logger;
-		private readonly ISendEndpointProvider _sendEndpointProvider;
 		private readonly IMediator _mediator;
 
-		public MediaCreatedEventConsumer(ILogger<MediaCreatedEventConsumer> logger, ISendEndpointProvider sendEndpointProvider, IMediator mediator)
+		public MediaCreatedEventConsumer(ILogger<MediaCreatedEventConsumer> logger, IMediator mediator)
 		{
 			_logger = logger;
-			_sendEndpointProvider = sendEndpointProvider;
 			_mediator = mediator;
 		}
 
 		public async Task Consume(ConsumeContext<MediaCreatedEvent> context)
 		{
-			ISendEndpoint sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMqQueues.StateMachine_UserMedia}"));
 
 			try
 			{
 				await _mediator.Publish(context.Message);
 
-				await sendEndpoint.Send(new UserMediaSuccessfulEvent((Guid)context.CorrelationId, context.Message.Id,context.Message.ImageOwnerId));
+				await context.RespondAsync(new MediaSuccessfulEvent((Guid)context.CorrelationId, context.Message.Id, context.Message.ImageOwnerId));
 
 			}
 			catch (Exception ex)
 			{
-				await sendEndpoint.Send(new UserMediaRejectedEvent(context.Message.CorrelationId, context.Message.Id,context.Message.ImageOwnerId, ex.Message));
+				await context.RespondAsync(new MediaRejectedEvent((Guid)context.CorrelationId, context.Message.Id, context.Message.ImageOwnerId, ex.Message));
+
 			}
 		}
 	}
