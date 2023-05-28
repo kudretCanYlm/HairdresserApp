46805b29615eb597c5d1c76f53d960d06c9c1313
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-28 16:26:02 +0300

    Add New Command

--- HairdresserService.Domain/Commands/HairdresserService/HairdresserServiceCommandHandler.cs
+++ HairdresserService.Domain/Commands/HairdresserService/HairdresserServiceCommandHandler.cs
@@ -1,30 +1,34 @@
 ﻿using AutoMapper;
+using Common.Media;
 using Events.HairdresserService;
-using FluentValidation.Results;
+using Events.MassTransitOptions;
 using HairdresserService.Domain.Interfaces;
 using HairdresserService.Domain.Models;
+using MassTransit;
 using MediatR;
 using NetDevPack.Messaging;
 
 namespace HairdresserService.Domain.Commands.HairdresserService
 {
 	public class HairdresserServiceCommandHandler : CommandHandler,
-													IRequestHandler<ActivateHairdresserServiceCommand, ValidationResult>,
-													IRequestHandler<CreateHairdresserServiceCommand, ValidationResult>,
-													IRequestHandler<DeactivateHairdresserServiceCommand, ValidationResult>,
-													IRequestHandler<DeleteHairdresserServiceCommand, ValidationResult>,
-													IRequestHandler<UpdateHairdresserServiceCommand, ValidationResult>
+													IRequestHandler<ActivateHairdresserServiceCommand, FluentValidation.Results.ValidationResult>,
+													IRequestHandler<CreateHairdresserServiceCommand, FluentValidation.Results.ValidationResult>,
+													IRequestHandler<DeactivateHairdresserServiceCommand, FluentValidation.Results.ValidationResult>,
+													IRequestHandler<DeleteHairdresserServiceCommand, FluentValidation.Results.ValidationResult>,
+													IRequestHandler<UpdateHairdresserServiceCommand, FluentValidation.Results.ValidationResult>
 	{
 		private readonly IHairdresserServiceRepository _hairdresserServiceRepository;
 		private readonly IMapper _mapper;
+		private readonly ISendEndpointProvider _sendEndpointProvider;
 
-		public HairdresserServiceCommandHandler(IHairdresserServiceRepository hairdresserServiceRepository, IMapper mapper)
+		public HairdresserServiceCommandHandler(IHairdresserServiceRepository hairdresserServiceRepository, IMapper mapper, ISendEndpointProvider sendEndpointProvider)
 		{
 			_hairdresserServiceRepository = hairdresserServiceRepository;
 			_mapper = mapper;
+			_sendEndpointProvider = sendEndpointProvider;
 		}
 
-		public async Task<ValidationResult> Handle(ActivateHairdresserServiceCommand request, CancellationToken cancellationToken)
+		public async Task<FluentValidation.Results.ValidationResult> Handle(ActivateHairdresserServiceCommand request, CancellationToken cancellationToken)
 		{
 			var hairdresserService = await _hairdresserServiceRepository.GetByIdAndHairdresserId(request.Id, request.HairdresserId);
 			
@@ -46,7 +50,7 @@ namespace HairdresserService.Domain.Commands.HairdresserService
 
 		}
 
-		public async Task<ValidationResult> Handle(CreateHairdresserServiceCommand request, CancellationToken cancellationToken)
+		public async Task<FluentValidation.Results.ValidationResult> Handle(CreateHairdresserServiceCommand request, CancellationToken cancellationToken)
 		{
 			var hairdresserService = _mapper.Map<HairdresserServiceModel>(request);
 
@@ -54,10 +58,27 @@ namespace HairdresserService.Domain.Commands.HairdresserService
 
 			hairdresserService.AddDomainEvent(_mapper.Map<HairdresserServiceCreatedEvent>(hairdresserService));
 
+			ISendEndpoint sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMqQueues.StateMachine_HairdresserServiceMedia}"));
+
+			foreach (var base64Media in request.Base64MediaList)
+			{
+				var mediaCreatedEvent = new HairdresserServiceMediaCreatedEvent
+				{
+					Id = Guid.NewGuid(),
+					FileExtension = MediaMethods.ToFileExtension(base64Media),
+					CustomType = MediaTypes.HAIRDRESSER_SERVICE_MULTI,
+					ImageOwnerId = hairdresserService.Id,
+					MediaData = MediaMethods.ToByteArray(base64Media),
+				};
+
+				await sendEndpoint.Send<HairdresserServiceMediaCreatedEvent>(mediaCreatedEvent);
+
+			}
+
 			return await Commit(_hairdresserServiceRepository.UnitOfWork);
 		}
 
-		public async Task<ValidationResult> Handle(DeactivateHairdresserServiceCommand request, CancellationToken cancellationToken)
+		public async Task<FluentValidation.Results.ValidationResult> Handle(DeactivateHairdresserServiceCommand request, CancellationToken cancellationToken)
 		{
 			var hairdresserService = await _hairdresserServiceRepository.GetByIdAndHairdresserId(request.Id, request.HairdresserId);
 
@@ -78,7 +99,7 @@ namespace HairdresserService.Domain.Commands.HairdresserService
 			return await Commit(_hairdresserServiceRepository.UnitOfWork);
 		}
 
-		public async Task<ValidationResult> Handle(DeleteHairdresserServiceCommand request, CancellationToken cancellationToken)
+		public async Task<FluentValidation.Results.ValidationResult> Handle(DeleteHairdresserServiceCommand request, CancellationToken cancellationToken)
 		{
 			var hairdresserService = await _hairdresserServiceRepository.GetByIdAndHairdresserId(request.Id, request.HairdresserId);
 
@@ -96,7 +117,7 @@ namespace HairdresserService.Domain.Commands.HairdresserService
 
 		}
 
-		public async Task<ValidationResult> Handle(UpdateHairdresserServiceCommand request, CancellationToken cancellationToken)
+		public async Task<FluentValidation.Results.ValidationResult> Handle(UpdateHairdresserServiceCommand request, CancellationToken cancellationToken)
 		{
 			var hairdresserService = await _hairdresserServiceRepository.GetByIdAndHairdresserId(request.Id, request.HairdresserId);
 
