d7613a4d7e3ecb07e69da59e7ed128517ab0e3ab
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-06-03 16:36:32 +0300

    Added Services

--- Notification.Api/Program.cs
+++ Notification.Api/Program.cs
@@ -1,25 +1,64 @@
+using Common.Cors;
+using Database.Repository.Redis;
+using Events.Appointment;
+using Events.MassTransitOptions;
+using Grpc.Auth;
+using Grpc.Hairdresser;
+using Grpc.HairdresserService;
+using Notification.Api.Hubs;
+using Notification.Api.Integrations.Consumers;
+using Notification.Api.Interfaces;
+using Notification.Api.RedisIndex;
+using Notification.Api.Repository;
+using Redis.OM;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddHairdresserGrpc(builder.Configuration["HairdresserUrl"]);
+builder.Services.AddAuthGrpc(builder.Configuration["AuthUrl"]);
+builder.Services.AddHairdresserServiceGrpc(builder.Configuration["HairdresserServiceUrl"]);
+builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
+builder.Services.AddSingleton<UserTracker>();
+builder.Services.AddSingleton(new RedisConnectionProvider(builder.Configuration["REDIS_CONNECTION_STRING"]));
+builder.Services.AddHostedService<IndexCreationService>();
+builder.Services.AddScoped(typeof(IRedisBaseRepository<>), typeof(RedisBaseRepository<>));
+builder.Services.AddMyMassTransit(builder.Configuration, Assembly.GetExecutingAssembly(),
+	(RabbitMqQueues.Appointment_AppointmentCreatedEventQueue, typeof(AppointmentCreatedEventConsumers)),
+	(RabbitMqQueues.Appointment_AppointmentApprovedEventQueue, typeof(AppointmentAppovedEventConsumers))
+	);
+
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: "AllowReact",
+					  builder =>
+					  {
+						  builder.WithOrigins("http://localhost:3000")
+						  .AllowAnyHeader()
+						  .AllowAnyMethod()
+						  .AllowCredentials();
+					  });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
+
+app.UseCors("AllowReact");
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapHub<NotificationHub>("hubs/notification");
+});
 
 app.Run();
