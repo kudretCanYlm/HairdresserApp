ae2db7192da052a936811e3dd6ad12ed99ab8a2c
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-06-03 16:15:53 +0300

    Add New GRPC Service And RabbitMq

--- Appointment.Api/Program.cs
+++ Appointment.Api/Program.cs
@@ -1,20 +1,27 @@
 using Appointment.Application.Extensions;
 using Appointment.Domain.Extensions;
 using Appointment.Infrastructure.Extensions;
+using Events.MassTransitOptions;
 using Events.Stores.MongoDb;
 using Grpc.Auth;
+using Grpc.Hairdresser;
+using Grpc.HairdresserService;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Swagger;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.UseDomain(typeof(Program));
 builder.Services.AddAuthGrpc(builder.Configuration["AuthUrl"]);
+builder.Services.AddHairdresserGrpc(builder.Configuration["HairdresserUrl"]);
+builder.Services.AddHairdresserServiceGrpc(builder.Configuration["HairdresserServiceUrl"]);
 builder.Services.AddAppointmentInfrastructure(x => x.UseSqlServer(builder.Configuration.GetConnectionString("AppointmentServiceDb")));
 builder.Services.AddAppointmentApplication();
 builder.Services.AddMongoDbEventStore(builder.Configuration);
-
+builder.Services.AddMyMassTransit(builder.Configuration, Assembly.GetExecutingAssembly());
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
