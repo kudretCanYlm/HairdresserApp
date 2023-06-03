58fc4dca4764ac39a5af35d7a927b15eec1fa686
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-06-03 16:20:24 +0300

    First Commit

---
+++ Appointment.Domain/Queries/Appointment/GetAppointmentByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Appointment.Domain.Models;
+using MediatR;
+
+namespace Appointment.Domain.Queries.Appointment
+{
+	public class GetAppointmentByIdQuery:IRequest<AppointmentModel>
+	{
+		public GetAppointmentByIdQuery(Guid id)
+		{
+			Id = id;
+		}
+
+		public Guid Id { get; set; }
+	}
+}
