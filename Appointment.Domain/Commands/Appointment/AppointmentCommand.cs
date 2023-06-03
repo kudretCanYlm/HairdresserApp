06ebf4b76e4aaaeea031f2da2fc7f4dd8cf1df36
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-06-03 16:16:32 +0300

    Add New Property

--- Appointment.Domain/Commands/Appointment/AppointmentCommand.cs
+++ Appointment.Domain/Commands/Appointment/AppointmentCommand.cs
@@ -10,7 +10,7 @@ namespace Appointment.Domain.Commands.Appointment
 		public string Notes { get; set; }
 		public DateTime AppointmentDate { get; set; }
 		public TimeSpan AppointmentStartTime { get; set; }
-		//public TimeSpan AppointmentEndTime { get; set; }
+		public TimeSpan AppointmentEndTime { get; set; }
 		public Guid UserId { get; set; }
 		public Guid HairdresserServiceId { get; set; }
 		public Guid HairdresserId { get; set; }
