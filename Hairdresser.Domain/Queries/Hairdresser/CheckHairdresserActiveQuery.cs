b0fb78a2a0030fb2f1e353810bacb8abc160c118
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-06-03 16:26:04 +0300

    first Commit

---
+++ Hairdresser.Domain/Queries/Hairdresser/CheckHairdresserActiveQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Hairdresser.Domain.Queries.Hairdresser
+{
+	public class CheckHairdresserActiveQuery:IRequest<bool>
+	{
+		public CheckHairdresserActiveQuery(Guid id, DateTime appointmentDate, TimeSpan appointmentStartTime, TimeSpan serviceDuration)
+		{
+			Id = id;
+			AppointmentDate = appointmentDate;
+			AppointmentStartTime = appointmentStartTime;
+			ServiceDuration = serviceDuration;
+		}
+
+		public Guid Id { get; set; }
+		public DateTime AppointmentDate { get; set; }
+		public TimeSpan AppointmentStartTime { get; set; }
+		public TimeSpan ServiceDuration { get; set; }
+	}
+}
